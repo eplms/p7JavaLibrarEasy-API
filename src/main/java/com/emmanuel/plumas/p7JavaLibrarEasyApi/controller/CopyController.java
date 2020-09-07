65fdd8bc823628d3feccfe05b66ced972ffb9339
Emmanuel Plumas <eplumas@free.fr>
2020-09-07 14:56:22 +0200

    maj methode controller requete POST de CopyController avec @RequestBody

--- src/main/java/com/emmanuel/plumas/p7JavaLibrarEasyApi/controller/CopyController.java
+++ src/main/java/com/emmanuel/plumas/p7JavaLibrarEasyApi/controller/CopyController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.emmanuel.plumas.p7JavaLibrarEasyApi.model.CopyEntity;
@@ -27,12 +28,12 @@ public class CopyController {
 		}
 		
 		@PostMapping(value="copy/create")
-		public void createCopy(CopyEntity copyEntity) {
+		public void createCopy(@RequestBody CopyEntity copyEntity) {
 			copyService.createCopy(copyEntity);
 		}
 		
 		@PostMapping(value="copy/update")
-		public void upDateCopy(CopyEntity copyEntity) {
+		public void upDateCopy(@RequestBody CopyEntity copyEntity) {
 			copyEntity=copyService.upDateCopy(copyEntity);
 		}
 		
